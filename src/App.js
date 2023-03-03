912cdca9338f1eeb7d8fa6b2ec6b9a645ad39b57
unknown <sb.bekish@gmail.com>
2023-03-03 14:45:59 +0600

    first1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  const personData = [{
+    name:"name:Jamaldin",
+    surname:"surname:Abdumutalib uulu",
+    age:"age:22",
+    car:"mers",
+  },
+{
+  name:"name:Malika",
+  surname:"surname:Abdumutalib kyzy",
+  age:"age:19",
+  car:"maybach",
+},
+{
+  name:"name:Nurbolot",
+  surname:"surname:Erkinbekov",
+  age:"age:17",
+  car:"velik",
+}]
+  return (
+    <div className="header">
+     <Header data={personData}></Header>
+    </div>
+  );
+}
+
+export default App;
